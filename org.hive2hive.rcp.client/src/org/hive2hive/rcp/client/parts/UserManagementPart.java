6fba00121f7f0406ae41e819b0cdfa628ffeb6f4
Nendor <marc.a.weber@gmx.ch>
2014-05-24 13:54:02 +0200

    Clean up of the UserManagementPart. Removed outdated elements.

--- org.hive2hive.rcp.client/src/org/hive2hive/rcp/client/parts/UserManagementPart.java
+++ org.hive2hive.rcp.client/src/org/hive2hive/rcp/client/parts/UserManagementPart.java
@@ -12,18 +12,13 @@ import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.hive2hive.rcp.client.bundleresourceloader.IBundleResourceLoader;
-import org.hive2hive.rcp.client.events.ConnectionStatus;
-import org.hive2hive.rcp.client.events.EventConstatns;
 import org.hive2hive.rcp.client.services.IUserService;
-import org.hive2hive.rcp.client.services.ServiceAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,26 +27,15 @@ public class UserManagementPart {
 	private static final Logger logger = LoggerFactory.getLogger(UserManagementPart.class);
 
 	@Inject
-	private IBundleResourceLoader resourceLoader;
-
-	@Inject
 	private IUserService userService;
 
 	@Inject
 	private IEventBroker eventBroker;
 
-	private Text txtUserId_reg;
-	private Text txtPassord_reg;
-	private Text txtPin_reg;
-
 	private Text txtUserId_login;
 	private Text txtPassword_login;
 	private Text txtPin_login;
 	private Text txtFileRoot;
-
-	private ImageViewerCached registrationLoader;
-	private ImageViewerCached loader;
-	private boolean userRegistered = false;
 
 	@PostConstruct
 	public void createControls(final Composite parent) {
@@ -59,7 +43,6 @@ public class UserManagementPart {
 
 		MigLayout layout = new MigLayout("", "[][]", "[top]");
 		parent.setLayout(layout);
-		createUserRegistrationGroup(parent);
 		createLoginGroup(parent);
 
 	}
@@ -112,123 +95,17 @@ public class UserManagementPart {
 		txtFileRoot.setLayoutData("grow, span");
 		txtFileRoot.setText("/Users/Nendor/H2H/MrX");
 
-		final ImageData[] imageData = resourceLoader.loadImageData(this.getClass(), "images/loader.gif");
-
-		loader = new ImageViewerCached(loginGroup);
-		loader.setImage(imageData[0]);
-		loader.setImages(imageData, 0);
-		loader.setVisible(false);
-
 		Button btnLoginUser = new Button(loginGroup, SWT.PUSH);
 		btnLoginUser.setText("Login user");
-		// btnLoginUser.setLayoutData("span");
 		btnLoginUser.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				logger.debug("Login user started.");
-				// boolean isUserRegistered = userService.isUserRegistered(txtUserId_login.getText());
-				// if (!isUserRegistered) {
-				// registerUser();
-				// }
-				//
-				// loader.setVisible(true);
-				Path userDirectoryPath = Paths.get(txtFileRoot.getText());
+				Path userDirPath = Paths.get(txtFileRoot.getText());
 				userService.registerAndLoginUser(txtUserId_login.getText(), txtPassword_login.getText(),
-						txtPin_login.getText(), Paths.get(txtFileRoot.getText()), eventBroker);
-				// userService.loginUser(txtUserId_login.getText(), txtPassword_login.getText(),
-				// txtPin_login.getText(),
-				// userDirectoryPath, new LoginUserListener());
-			}
-
-			private void registerUser() {
-				logger.debug("Registering user '{}'", txtUserId_login.getText());
-				eventBroker.post(EventConstatns.PROGRESS_INFORMATION,
-						String.format("Registering user %s", txtUserId_login.getText()));
+						txtPin_login.getText(), userDirPath, eventBroker);
 			}
 		});
-	}
-
-	@Deprecated
-	private void createUserRegistrationGroup(final Composite parent) {
-		Group userRegistrationGroup = new Group(parent, SWT.SHADOW_ETCHED_IN);
-		MigLayout userRegistrationlayout = new MigLayout("wrap", "[right]5[]", "");
-		userRegistrationGroup.setLayout(userRegistrationlayout);
-		userRegistrationGroup.setText("User Registration");
-
-		Label lblUserID = new Label(userRegistrationGroup, SWT.NONE);
-		lblUserID.setText("User ID:");
-		txtUserId_reg = new Text(userRegistrationGroup, SWT.SINGLE | SWT.BORDER | SWT.DOUBLE_BUFFERED);
-		txtUserId_reg.setText("Mr. X");
-
-		Label lblPassword = new Label(userRegistrationGroup, SWT.NONE);
-		lblPassword.setText("Password:");
-		txtPassord_reg = new Text(userRegistrationGroup, SWT.SINGLE | SWT.BORDER | SWT.DOUBLE_BUFFERED);
-		txtPassord_reg.setText("secret");
-
-		Label lblPin = new Label(userRegistrationGroup, SWT.NONE);
-		lblPin.setText("Pin:");
-		txtPin_reg = new Text(userRegistrationGroup, SWT.SINGLE | SWT.BORDER | SWT.DOUBLE_BUFFERED);
-		txtPin_reg.setText("1234");
-
-		final ImageData[] imageData = resourceLoader.loadImageData(this.getClass(), "images/loader.gif");
-
-		registrationLoader = new ImageViewerCached(userRegistrationGroup);
-		registrationLoader.setImage(imageData[0]);
-		registrationLoader.setImages(imageData, 0);
-		registrationLoader.setVisible(false);
-
-		Button btnRegisterUser = new Button(userRegistrationGroup, SWT.PUSH);
-		btnRegisterUser.setText("Register user");
-		btnRegisterUser.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				toggleRegistrationLoader();
-				if (!userRegistered) {
-					userService.registerUser(txtUserId_reg.getText(), txtPassord_reg.getText(), txtPin_reg.getText(),
-							new RegisterUserListener());
-				}
-			}
-		});
-	}
-
-	private void toggleRegistrationLoader() {
-		registrationLoader.setVisible(!registrationLoader.getVisible());
-	}
-
-	private class RegisterUserListener extends ServiceAdapter {
-
-		@Override
-		public void serviceFinished() {
-			logger.debug("User creation finished.");
-			toggleRegistrationLoader();
-		}
-
-		@Override
-		public void serviceSucceeded() {
-			logger.debug("User creation sucessful.");
-			userRegistered = true;
-			eventBroker.post(EventConstatns.USER_STATUS, ConnectionStatus.CONNECTED);
-		}
-
-	}
-
-	private class LoginUserListener extends ServiceAdapter {
-
-		@Override
-		public void serviceFinished() {
-			logger.debug("User login service finished");
-			loader.setVisible(!loader.isVisible());
-		}
-
-		@Override
-		public void serviceSucceeded() {
-			logger.debug("User login was successful.");
-		}
-
-		@Override
-		public void serviceFailed() {
-			logger.debug("Service failed!");
-		}
 	}
 
 }
